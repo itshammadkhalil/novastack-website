fd52cc2b8343954ff38846fa93b674e1b1c6b63f
Hammad Khalil <hammadkhalil.me@gmail.com>
2026-03-03 05:11:22 +0500

    chore: updated component sequence and finalized App.jsx structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Hero from './Hero';
-import AllServices from './AllServices';
 import Expertise from './Expertise';
+import AllServices from './AllServices';
 import Portfolio from './Portfolio';
 import Team from './Team';
 import Contact from './Contact'; 
@@ -13,10 +13,11 @@ function App() {
     <div className="bg-zinc-950 min-h-screen flex flex-col overflow-x-hidden selection:bg-green-500/30">
       <Navbar />
       
-      <main className="flex-1 w-full relative">
+     
+      <main className="flex-1 w-full relative z-auto">
         <Hero />
-        <AllServices />
         <Expertise />
+        <AllServices />
         <Portfolio />
         <Team />
         <Contact /> 
